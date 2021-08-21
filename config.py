87dc8011f638f8f03e5c177cd1a92202e40b7a33
ouedraogoluc <ouedraogoluc664@gmail.com>
2021-08-21 21:47:09 +0100

    deploiement

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 #database initialization
 if os.environ.get('DATABASE_URL') is None:
     # get connexion from postgresql
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:root@localhost:5432/product_db" # postgres://nomUtilisateur:motpasse@ipserver:port/basedbname
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:luc@localhost:5432/product_db" # postgres://nomUtilisateur:motpasse@ipserver:port/basedbname
 else:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("postgres://", "postgresql://", 1)
 
