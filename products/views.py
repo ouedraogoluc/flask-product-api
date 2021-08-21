e1f8b71131784d50c2c33925b340433490c03318
EspoirNorbert <espoirnorbert@gmail.com>
2021-08-22 00:31:11 +0200

    v7

--- products/views.py
+++ products/views.py
@@ -72,5 +72,5 @@ def get_product_image(filename):
     print(request.url_root + url_for('get_product_image', filename=filename))
     extension = filename.split('.')[1]
     # get full path
-    file_path = '..\\static\\uploads\\'+ filename
+    file_path = '..\\..\\static\\uploads\\'+ filename
     return send_file(file_path, mimetype='image/'+ extension)
